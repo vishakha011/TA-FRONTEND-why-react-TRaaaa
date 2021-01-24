457f64fdd654a0d376813851af0eabf87fb025b5
vishakha011 <khatadevishakha01@gmail.com>
2021-01-24 21:13:18 +0530

    Article App

---
+++ block-BRaaao/code/article-app/src/components/Hero.js
@@ -0,0 +1,12 @@
+function Hero() {
+    return (
+        <div className="container hero">
+            <img className="hero-img" src="/images/hero.jpg" alt="Hero"></img>
+            <p className="tag-line">The more that you read, the more things you will know. The more that you learn, the more places you'll go.</p>
+        </div>
+        
+    )
+};
+
+
+export default Hero;
