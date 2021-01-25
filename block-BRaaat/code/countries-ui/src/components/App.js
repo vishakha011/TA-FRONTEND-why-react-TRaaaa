1878ef85b8235b90b520bbcc9221fc3dd5937596
vishakha011 <khatadevishakha01@gmail.com>
2021-01-25 18:01:35 +0530

    Countries UI

---
+++ block-BRaaat/code/countries-ui/src/components/App.js
@@ -0,0 +1,17 @@
+import Cards from "./Card"
+
+function App() {
+    return (
+        <>
+        <h1 className="heading">Countries UI</h1>
+        <div className="container">
+            <ul className="list">
+                <Cards />
+            </ul>
+        </div>
+        </>
+    )
+}
+
+
+export default App;
