fc9da09a2c9f05cb7b928cbf528dfdf524f3b174
vishakha011 <khatadevishakha01@gmail.com>
2021-01-27 08:40:49 +0530

    Button Design System

---
+++ block-BRaaax/code/button-app/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Button from "./components/App";
+import "./styles/main.css"
+
+
+ReactDOM.render(<Button />, document.getElementById("root"))
