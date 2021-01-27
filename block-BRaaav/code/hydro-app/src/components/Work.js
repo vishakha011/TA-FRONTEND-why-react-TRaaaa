6ccfb5339d916a835054a79d34c82331e083af36
vishakha011 <khatadevishakha01@gmail.com>
2021-01-27 08:13:37 +0530

    HTML to React

---
+++ block-BRaaav/code/hydro-app/src/components/Work.js
@@ -0,0 +1,51 @@
+function Work() {
+    return (
+        <>
+            <section className="work padding">
+                <div className="container">
+                <header className="sec-header text-center">
+                    <h2 className="heading">Our Work</h2>
+                    <div className="dot-wrapper">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    </div>
+                </header>
+                <div className="flex">
+                    <div className="flex-23">
+                    <img
+                        className="flexible-img"
+                        src="/images/work-image1.jpg"
+                        alt="Work 1"
+                    />
+                    </div>
+                    <div className="flex-23">
+                    <img
+                        className="flexible-img"
+                        src="/images/work-image2.jpg"
+                        alt="Work 2"
+                    />
+                    </div>
+                    <div className="flex-23">
+                    <img
+                        className="flexible-img"
+                        src="/images/work-image3.jpg"
+                        alt="Work 3"
+                    />
+                    </div>
+                    <div className="flex-23">
+                    <img
+                        className="flexible-img"
+                        src="/images/work-image4.jpg"
+                        alt="Work 4"
+                    />
+                    </div>
+                </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default Work;
